1eba23542b4fe6a47d6e2c3a04adbc81d8515d77
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-15 08:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ class Square extends Polygon{
   get isValid(){
     let valid = true;
     if(this.sides.length == 4) {
-      if(this.sides[0] != this.sides[1] || this.sides[1] != this.sides[2] || this.sides[2] != this.sides[3] || this.sides[3] != this.sides[0] ||) valid = false;
+      if(this.sides[0] != this.sides[1] || this.sides[1] != this.sides[2] || this.sides[2] != this.sides[3] || this.sides[3] != this.sides[0]) valid = false;
       return valid;
     }
     else return false;
